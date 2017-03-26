adfeecfa812e0cc68aa5a041988ccebf3dd0455c
Aaron Killen <akillen@gmail.com>
2017-03-26 14:50:11 -0400

    Added ejs views

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const express = require('express')
-const app = express()
-const words = require('./lib/words.js')
-const fs = require('fs')
+const express = require('express');
+const app = express();
+const words = require('./lib/words.js');
+const fs = require('fs');
 
 const dictionary = JSON.parse(
   fs.readFileSync('./lib/dictionary.json')
 ).dictionary;
 
+app.set('view engine', 'ejs');
+app.set('view options', {layout: false});
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.render('index', {});
+});
 
 app.post('/search', (req, res) => {
   res.send(words.search('d_g', dictionary));
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
